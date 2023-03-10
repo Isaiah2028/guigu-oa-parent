df9a274b21353224ea4e5683e2865ef6b24a5b8b
luyuan <luyuan@egoonet.com>
2023-03-10 13:37:17 +0800

    继承Knife4j框架做swagger测试，编写完成角色管理的相关接口

--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -3,10 +3,17 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.service.SysSysRoleService;
 import com.atguigu.model.system.SysRole;
 import com.atguigu.result.Result;
+import com.atguigu.vo.system.SysRoleQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +21,7 @@ import java.util.List;
  * @Author: IsaiahLu
  * @date: 2023/3/10 0:08
  */
+@Api(tags = "角色管理接口")
 @RestController
 @RequestMapping(value = "/admin/system/sysRole", produces = "application/json")
 public class SysRoleController {
@@ -22,21 +30,103 @@ public class SysRoleController {
     SysSysRoleService sysSysRoleService;
 
 
-
+    @ApiOperation("测试接口")
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String test() {
         return "wusisi";
     }
 
+    @ApiOperation("查询所有角色")
     @RequestMapping(path = "/findAll", method = RequestMethod.GET)
-    public Result findAll(){
-
+    public Result findAll() {
         List<SysRole> list = sysSysRoleService.list();
         System.out.println("list: " + list);
-
         return Result.ok(list);
     }
 
+
+    @ApiOperation("分页查询角色")
+    @RequestMapping(path = "/{page}/{limit}", method = RequestMethod.GET)
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable Long limit,
+                                SysRoleQueryVo sysRoleQueryVo) {
+
+        Page<SysRole> pageParam = new Page<>(page, limit);
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<SysRole>();
+        String roleName = sysRoleQueryVo.getRoleName();
+
+        //条件查询参数不为空
+        if (!StringUtils.isBlank(roleName)) {
+            wrapper.like(SysRole::getRoleName, roleName);
+        }
+        IPage<SysRole> pageModel = sysSysRoleService.page(pageParam, wrapper);
+
+        return Result.ok(pageModel);
+    }
+
+    @ApiOperation("添加角色")
+    @RequestMapping(path = "/addRole", method = RequestMethod.POST)
+    public Result addRole(@RequestBody SysRole sysRole) {
+
+        boolean isAdded = sysSysRoleService.save(sysRole);
+        if (isAdded) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据id查询角色")
+    @RequestMapping(path = "/getRole/{id}", method = RequestMethod.GET)
+    public Result queryById(@PathVariable Long id) {
+
+        SysRole sysRole = sysSysRoleService.getById(Long.valueOf(id));
+
+        if (!ObjectUtils.isEmpty(sysRole)) {
+
+            return Result.ok(sysRole);
+        } else {
+            return Result.fail("id错误或用户不存在...");
+        }
+
+    }
+
+    @ApiOperation("修改角色")
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    public Result updateRole(@RequestBody SysRole sysRole) {
+
+        boolean isUpdated = sysSysRoleService.updateById(sysRole);
+        if (isUpdated) {
+            return Result.ok(sysRole);
+        } else {
+            return Result.fail("修改角色失败...");
+        }
+    }
+
+    @ApiOperation("根据id删除")
+    @RequestMapping(path = "/delete/{id}", method = RequestMethod.GET)
+    public Result deleteById(@PathVariable Long id) {
+
+        boolean isDeleted = sysSysRoleService.removeById(id);
+        if (isDeleted) {
+            return Result.ok(isDeleted);
+        } else {
+            return Result.fail("删除失败...");
+        }
+    }
+
+    @ApiOperation("根据批量id删除")
+    @RequestMapping(path = "/deleteByIds", method = RequestMethod.DELETE)
+    public Result deleteByIds(@RequestBody List<Long> idList) {
+
+        boolean isDeleted = sysSysRoleService.removeByIds(idList);
+        if (isDeleted) {
+            return Result.ok(isDeleted);
+        } else {
+            return Result.fail("批量删除失败...");
+        }
+
+    }
 
 
 }
--- service-oa/src/main/java/com/atguigu/auth/mapper/SysRoleMapper.java
+++ service-oa/src/main/java/com/atguigu/auth/mapper/SysRoleMapper.java
@@ -1,15 +1,9 @@
 package com.atguigu.auth.mapper;
 
 import com.atguigu.model.system.SysRole;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @Author: IsaiahLu
